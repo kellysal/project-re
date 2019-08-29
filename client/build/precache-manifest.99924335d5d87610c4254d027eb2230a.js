d2a4b8fce9e85e4ecfb9d4e60c675c18e1f9311a
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-08-29 19:38:14 -0400

    database switch

---
+++ client/build/precache-manifest.99924335d5d87610c4254d027eb2230a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e79530e125d1fa81f95",
+    "url": "/static/css/main.1ebfb004.chunk.css"
+  },
+  {
+    "revision": "7e79530e125d1fa81f95",
+    "url": "/static/js/main.fffa3351.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d30a0465812cd5ea69f6",
+    "url": "/static/js/2.8bf96eed.chunk.js"
+  },
+  {
+    "revision": "531fd56590ccddfe6191f20286340c2f",
+    "url": "/static/media/header.531fd565.jpg"
+  },
+  {
+    "revision": "7de22fb53058ba31528c3e6d4ea6d431",
+    "url": "/static/media/bostonimg.7de22fb5.jpg"
+  },
+  {
+    "revision": "d23a1687073bd8779b8fa55d9ee3d243",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -17,7 +17,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/relisting", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/relisting", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_kv1qjt9g:5pis301lrvvkpja0pn9s8t70on@ds311968.mlab.com:11968/heroku_kv1qjt9g", { useNewUrlParser: true });
 
 // mongoose.Promise = Promise;
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
